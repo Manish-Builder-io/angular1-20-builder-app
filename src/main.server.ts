17c38ce420e004879848e61035f04eafe4cde780
Manish-Builder-io <manish@builder.io>
2025-10-24 18:31:39 +0530

    Fix server-side bootstrap context for Angular 19

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { platformServer } from '@angular/platform-server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => {
+  const platform = platformServer();
+  return bootstrapApplication(AppComponent, {
+    ...config,
+    providers: [
+      ...config.providers || [],
+      platform.providers
+    ]
+  });
+};
 
 export default bootstrap;
